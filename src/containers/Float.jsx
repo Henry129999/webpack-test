8ab453683dab1b19ed3ddd7d40b0ee7b30aaced8
huanghao04 <huanghao04@fangdd.com>
2019-11-20 10:37:37 +0800

    新的提交

---
+++ src/containers/Float.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Float.css';
+
+export default function Float() {
+  return (
+    <div className={styles.box}>
+      <div className={styles.item1}>111</div>
+      <div className={styles.item2}>222</div>
+      <div className={styles.item3}>333</div>
+    </div>
+  );
+}
--- src/routers.jsx
+++ src/routers.jsx
@@ -6,11 +6,11 @@ import { createBrowserHistory as createHistory } from "history";
 import Main from './containers/Main.jsx';
 import Footer from './containers/Footer.jsx';
 import Test_page from './containers/Test_page.jsx';
+import Float from './containers/Float.jsx';
 
 export default function Routers() {
 
   function getAsyncComponent(load) {
-    console.log('load', load);
     return class AsyncComponent extends PureComponent {
 
       componentDidMount() {
@@ -19,7 +19,7 @@ export default function Routers() {
           // 代码加载成功，获取到了代码导出的值，调用 setState 通知高阶组件重新渲染子组件
           this.setState({
             component,
-          })
+          });
         });
       }
 
@@ -28,7 +28,7 @@ export default function Routers() {
         // component 是 React.Component 类型，需要通过 React.createElement 生产一个组件实例
         return component ? createElement(component) : null;
       }
-    }
+    };
   }
 
   return (
@@ -36,7 +36,7 @@ export default function Routers() {
       <Switch>
         <Route exact path="/" component={Main}/>
         <Route path="/footer" component={Footer}/>
-        // 使用按需加载组件
+        /* 使用按需加载组件 */
         <Route path='/about' component={getAsyncComponent(
           // 异步加载函数，异步地加载 About 组件
           () => import(/* webpackChunkName: 'asyncComponent-about' */'./asyncComponent/About')
@@ -48,6 +48,7 @@ export default function Routers() {
         )}
         />
         <Route path="/test_page" component={Test_page}/>
+        <Route path="/float" component={Float}/>
       </Switch>
     </Router>
   );
